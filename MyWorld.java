99591e52afeb6959b3f7b29a07140f6f446262af
TheLostone145 <martinthegamer145@gmail.com>
2024-09-19 13:07:48 -0400

    Fixed Error

--- MyWorld.java
+++ MyWorld.java
@@ -23,7 +23,7 @@ public class MyWorld extends World
         addObject(elephant, 300, 300);
         
         
-        Label scoreLabel= new Label(0,80);
+        scoreLabel= new Label(0,80);
         addObject(scoreLabel, 50, 50);
         
         
